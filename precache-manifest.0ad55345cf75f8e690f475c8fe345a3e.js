819e6128001e41c54c68ed0aa7110c395c512843
Tushar saxena <46051734+tsaxena4k@users.noreply.github.com>
2020-09-01 09:39:45 +0530

    Updates

---
+++ precache-manifest.0ad55345cf75f8e690f475c8fe345a3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ece252ac438a5b9671ca4bb44401ef",
+    "url": "/ToDo-List/index.html"
+  },
+  {
+    "revision": "4e4e05d262df8cd964b4",
+    "url": "/ToDo-List/static/css/main.9354c5ed.chunk.css"
+  },
+  {
+    "revision": "b60e5126e1c8fa0fdd09",
+    "url": "/ToDo-List/static/js/2.20f9a1c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-List/static/js/2.20f9a1c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e4e05d262df8cd964b4",
+    "url": "/ToDo-List/static/js/main.bf9bcdee.chunk.js"
+  },
+  {
+    "revision": "217cc7b3b5c1aec9c97c",
+    "url": "/ToDo-List/static/js/runtime-main.55f1b8b8.js"
+  }
+]);
